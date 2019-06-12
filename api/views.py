a525aee3839ddf083f49405c95bcf66a80f72c8c
mar <tejedamarcia@gmail.com>
2019-06-12 00:30:09 -0300

    posting list of files

--- api/views.py
+++ api/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
-
-from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
+
+from core.models import File
 from .serializers import FileSerializer
+
 
 # This will return a list of books
 @api_view(["GET"])
 def file(request):
-    files = ["Pro Python", "Fluent Python", "Speaking javascript", "The Go programming language"]
-    return Response(status=status.HTTP_200_OK, data={"data": files})
+    #files = ["Pro Python", "Fluent Python", "Speaking javascript", "The Go programming language"]
+    files = File.objects.all()
+    serializer = FileSerializer(files, many=True)
+    return JsonResponse(serializer.data, safe=False)
+    #return Response(status=status.HTTP_200_OK, data={"data": files})
 
 class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
