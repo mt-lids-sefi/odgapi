243cbe488bc3772133559162dee234d0f7d37030
mar <tejedamarcia@gmail.com>
2019-10-31 18:30:17 -0300

    kmeansstrategy

--- core/model/clusterizer/KMeansStrategy.py
+++ core/model/clusterizer/KMeansStrategy.py
@@ -1,12 +1,23 @@
+from pandas import DataFrame
+from sklearn.cluster import KMeans
 from core.model.clusterizer import ClusterStrategy
 
 class KMeansStrategy(ClusterStrategy):
 
     def __init__(self):
-        self.distance= 3
+        self.k = 3
 
-    def clusterize(self, lFile, colA, colB):
-        pass
+#las columnas que se reciben ya están caterogizadas
+    def clusterize(self, ds, col_a, col_b):
+        dataset = ds.get_data()
+        df = DataFrame()
+        df['c1'] = dataset[col_a]
+        df['c2'] = dataset[col_b]
+        X = df[['c1', 'c2']].values
+        kmeans = KMeans(n_clusters=self.k).fit(X)
+        centroids = kmeans.cluster_centers_
+        labels = kmeans.labels_
+        return [centroids, labels]
 
-    def setDistance(self, distance):
-        self.distance = distance
+    def set_k(self, k):
+        self.k = k
