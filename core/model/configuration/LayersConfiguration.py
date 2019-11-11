ad23f9b442884ef5cc14dcb91b11add0b3389f5e
mar <tejedamarcia@gmail.com>
2019-11-11 18:11:23 -0300

    layers conf

--- core/model/configuration/LayersConfiguration.py
+++ core/model/configuration/LayersConfiguration.py
@@ -1,15 +1,16 @@
 from django.db import models
 from picklefield import PickledObjectField
 
+from core.model.configuration.Configuration import Configuration
 from core.model.files.IDataSource import IDataSource
 
 
-class LayersConfiguration:
+class LayersConfiguration(Configuration):
 
     ds_a = models.ForeignKey(IDataSource, on_delete=models.CASCADE, related_name='+', null=True)
     ds_b = models.ForeignKey(IDataSource, on_delete=models.CASCADE, related_name='+', null=True)
     popup_data = PickledObjectField(null=True)
     colours = PickledObjectField(null=True)
 
-    def __init__(self):
-        pass
+    def set_colours(self, colours):
+        self.colours = colours
---
+++ core/model/configuration/__init__.py
@@ -0,0 +1,1 @@
+from .LayersConfiguration import LayersConfiguration
