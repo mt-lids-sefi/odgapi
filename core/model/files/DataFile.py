aea76f6c1af0200d83b94db1cf3d6d1c2f724e43
marcia-t <tejedamarcia@gmail.com>
2019-11-13 16:58:12 -0300

    minor fix on saving datafiles

--- core/model/files/DataFile.py
+++ core/model/files/DataFile.py
@@ -1,12 +1,17 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
 from core.model.files.IDataSource import IDataSource
-
+import pandas as pd
 
 class DataFile(IDataSource):
     doc = models.FileField(upload_to='files/', verbose_name='Archivo',
                            validators=[FileExtensionValidator(allowed_extensions=['csv'])])
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        dataset = pd.read_csv(self.doc, error_bad_lines=False)
+        self.dataset = dataset
+        super().save(*args, **kwargs)  # Call the "real" save() method.
 
     def set_doc(self, doc):
         self.doc = doc
