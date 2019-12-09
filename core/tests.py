cbcd881f55a457f2fb707a8c41d134e9352ad2cc
marcia-t <tejedamarcia@gmail.com>
2019-12-09 17:30:40 -0300

    starting to tes

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
+from csv import reader
+from io import StringIO
+
 from django.test import TestCase
 
+
 # Create your tests here.
+from core.model.files.GeoFile import GeoFile
+
+
+class GeoFileTest(TestCase):
+
+    def setUp(self):
+
+        self.geofile = GeoFile.objects.create(name='geofile test', description='description', lat_col='latitude', lon_col='longitude', doc='files/GeoVin_sample.csv')
+        self.geofile.save()
+
+    def tearDown(self):
+        self.geofile.delete()
+
+    def test_name_desc(self):
+        self.assertEqual(self.geofile.name, 'geofile test')
