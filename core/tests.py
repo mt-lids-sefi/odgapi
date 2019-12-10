30caad4efdf22594b25db1b1a58b7a820a6a23f3
mar <tejedamarcia@gmail.com>
2019-12-10 08:59:40 -0300

    test

--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,6 @@
-from csv import reader
-from io import StringIO
-
+import pandas as pd
 from django.test import TestCase
-
+import numpy as np
 
 # Create your tests here.
 from core.model.files.GeoFile import GeoFile
@@ -11,12 +9,25 @@ from core.model.files.GeoFile import GeoFile
 class GeoFileTest(TestCase):
 
     def setUp(self):
-
-        self.geofile = GeoFile.objects.create(name='geofile test', description='description', lat_col='latitude', lon_col='longitude', doc='files/GeoVin_sample.csv')
-        self.geofile.save()
+        self.gf = GeoFile.objects.create(name='geofile test', description='description', lat_col='latitude', lon_col='longitude', doc='files/GeoVin_sample.csv')
 
     def tearDown(self):
-        self.geofile.delete()
+        self.gf.delete()
 
     def test_name_desc(self):
-        self.assertEqual(self.geofile.name, 'geofile test')
+        self.assertEqual(self.gf.name, 'geofile test')
+        self.assertEqual(self.gf.description, 'description')
+
+    def test_dataset(self):
+        lat = self.gf.lat_col
+        lon = self.gf.lon_col
+        df = pd.read_csv('media/files/GeoVin_sample.csv', error_bad_lines=False)
+        df[lon] = df[lon].replace(r'\s+', np.nan, regex=True)
+        df[lon] = df[lon].replace(r'^$', np.nan, regex=True)
+        df[lon] = df[lon].fillna(-0.99999)
+        df[lon] = pd.to_numeric(df[lon])
+        df[lat] = df[lat].replace(r'\s+', np.nan, regex=True)
+        df[lat] = df[lat].replace(r'^$', np.nan, regex=True)
+        df[lat] = df[lat].fillna(-0.99999)
+        df[lat] = pd.to_numeric(df[lat])
+        self.assertEqual(self.gf.dataset, df)
